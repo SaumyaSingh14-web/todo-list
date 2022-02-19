415c1cf01b6d5c4aa60ed14490d90a1482ccb360
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-02-19 19:23:38 +0530

    title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Card from "./components/Card";
-
 function App() {
   return (
-    <div className="flex justify-center">
-      <Card />
+    <div className="App">
+      <div className="flex justify-center font-bold mt-10">
+        {" "}
+        TODO-List
+        <div className="flex justify-center">
+          <form action="" className="bg-white rounded-sm"></form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
+import Inputbox from "./Inputbox";
+
 function Card(){
     return(
-        <div className="bg-white flex justify-center min-h-screen w-1/2 border-2 shadow-sm">
-            <div className="bg-blue-400 h-min min-w-full text-center p-3">TODO-list</div>
-            
+        <div>
+           <div className="flex justify-center h-10 w-7 bg-white"></div> 
         </div>
     );
 }
