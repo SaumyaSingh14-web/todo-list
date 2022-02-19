7bd2b7ce9941d709da1b242d4c28d69e60c5fda8
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-02-19 17:08:02 +0530

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="bg-white flex justify-center min-h-screen w-1/2 border-2 shadow-sm">
+            <div className="bg-blue-400 h-min min-w-full text-center p-3">TODO-list</div>
+            
+        </div>
+    );
+}
+
+export default Card;
