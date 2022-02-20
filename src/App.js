673ce73dd8d69f140e27f2063a679e2e2864b37c
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2022-02-20 12:30:58 +0530

    storing the input data into an array by clicking on button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
 import Button from "./components/Button";
 
 function App() {
+  const [task, setTask] = useState([]); //to store the submitted data
+
+  const submitHandler = (e) => {
+    console.log("submitted");
+    e.preventDefault();
+    setTask([...task, task]); //storing in an array without losing previous data
+  };
+
   return (
     <div className="App bg-gray-100">
       <div className="text-gray-900 text-center p-14 font-extrabold">
         TODO-List
         <div className="flex justify-center mt-4 pb-5 ml-3">
           <form
+            onSubmit={submitHandler}
             action=""
             className="bg-white shadow-md rounded-sm sm:p-36 sm:pt-6 lg:p-60 lg:pt-10 min-h-screen border-2"
           >
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button(props){
     return(
-        <div className="bg-blue-500 rounded-md border hover:bg-blue-600 hover:outline hover:outline-blue-500 shadow-xl w-3/12 p-2 text-xs focus:outline-none focus:shadow-sm"><button>{props.btn}</button></div>
+        <div className="bg-blue-400 rounded-md border hover:bg-blue-500 hover:outline hover:outline-blue-200 shadow-md w-3/12 p-2 text-xs focus:outline-none focus:shadow-sm"><button>{props.btn}</button></div>
     );
 }
 
